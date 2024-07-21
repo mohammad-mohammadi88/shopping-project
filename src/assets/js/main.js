786a879dafc818c51e581c08b2fe0e5206da4832
mohammad-mohammadi88 <m.mohammadi8811@gmail.com>
2024-07-21 23:31:40 +0330

    sticky navbar done

---
+++ src/assets/js/main.js
@@ -0,0 +1,3 @@
+'use strict';
+addEventListener('scroll',()=>{document.getElementById('navSticky').style.opacity = document.documentElement.scrollTop > 95 ? '0.6' : '1'});
+barsOpen.onclick = ()=>{document.querySelector('#navSticky ul').classList.toggle('nav-sticky-block')}
