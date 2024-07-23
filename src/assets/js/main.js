98ad494df53dcba0f263aa2b019a5758a2ff6f90
mohammad-mohammadi88 <m.mohammadi8811@gmail.com>
2024-07-23 18:54:13 +0330

    hero header part done

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,15 @@
 'use strict';
 addEventListener('scroll',()=>{document.getElementById('navSticky').style.opacity = document.documentElement.scrollTop > 95 ? '0.6' : '1'});
 barsOpen.onclick = ()=>{document.querySelector('#navSticky ul').classList.toggle('nav-sticky-block')};
+assignFormOpen.onclick = ()=>{
+    assignForm.hidden = false;
+    modalFooterAssign.hidden = false;
+    modalFooterLogin.hidden = true;
+    loginForm.hidden = true
+}
+loginFormOpen.onclick = ()=>{
+    assignForm.hidden = true;
+    modalFooterAssign.hidden = true;
+    modalFooterLogin.hidden = false;
+    loginForm.hidden = false
+}
