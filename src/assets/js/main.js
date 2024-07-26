b931f938e9d6e9876b069e80e49c17cfe0688b0d
mohammad-mohammadi88 <m.mohammadi8811@gmail.com>
2024-07-26 19:02:53 +0330

    mini card part done

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,5 @@
 'use strict';
-addEventListener('scroll',()=>{document.getElementById('navSticky').style.opacity = document.documentElement.scrollTop > 95 ? '0.6' : '1'});
-barsOpen.onclick = ()=>{document.querySelector('#navSticky ul').classList.toggle('nav-sticky-block')};
+barsOpen.onclick = ()=>{document.querySelector('#navSticky').classList.toggle('nav-sticky-block');};
 assignFormOpen.onclick = ()=>{
     assignForm.hidden = false;
     modalFooterAssign.hidden = false;
